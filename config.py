14960f921833f004756114eaad7146f31bc36954
Lalit-Mahajan <139694592+Lalit-Mahajan@users.noreply.github.com>
2026-01-20 13:32:52 +0530

    first commit

--- config.py
+++ config.py
@@ -1,9 +1,11 @@
-import os
 # config.py
+import os
 from pymongo import MongoClient
 
-# ================= MONGODB (BANK APP)
-MONGO_URI = "mongodb+srv://lalitmahajan484_db_user:EjYIfU3Zi7SvzCNK@cluster0.0atbyqx.mongodb.net/Bank_db"
+MONGO_URI = os.getenv(
+    "MONGO_URI",
+    "mongodb+srv://lalitmahajan484_db_user:EjYIfU3Zi7SvzCNK@cluster0.0atbyqx.mongodb.net/Bank_db"
+)
 
 mongo_client = MongoClient(MONGO_URI)
 bank_db = mongo_client.Bank_db
